ce58d6a311aa0a45e8ddd8c69fb704d03c31ac14
JagMolar <juana.garmue@educa.jcyl.es>
2020-05-16 10:30:35 +0200

    primeros controles

---
+++ Kata1/main.py
@@ -0,0 +1,22 @@
+'''
+Los comentarios de bloque van con 3 comillas simples
+'''
+
+# variables sin ; final
+nombre = 'Juan García'
+edad= 46
+esta_trabajando = True
+dato = input('introduce un dato')
+print(dato)
+
+edad = edad + 1
+
+print(edad)
+
+# Para las condiciones se hace con el tabulador
+if(edad >= 18):
+    nombre2 = input('tu nombre: ')
+    print(nombre2 + ' ' + 'eres mayor de edad')
+else:
+    print('No eres mayor de edad')
+
